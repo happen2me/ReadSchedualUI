7e201113a1cbe76cacacf907f2cd3a704e403442
Yuanchun Shen <immr.shen@gmail.com>
2018-06-02 00:53:48 +0800

    Fill a simple expense page

--- pages/expense/expense.js
+++ pages/expense/expense.js
@@ -1,25 +1,50 @@
 // pages/expense/expense.js
+
+var chart = require("../../utils/chart.js");
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    /* 请参阅 https://github.com/ioneday/wxchart */
+    chart.draw(this, 'canvas1', {
+      hideYaxis: false,
+      color: ['#394655', '#74DAE5', '#ED7672', '#F3AA59', '#FEE746'],
+      title: {
+        text: "年支出",
+        color: "#333333",
+        size: 16
+      },
+      xAxis: {
+        color: "#666A73",
+        size: 10,
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月' , '12月']
+      },
+      series: [
+        {
+          name: " ",
+          category: "bar", //切换柱状图或折线图
+          data: [37, 63, 60, 78, 92, 63, 57, 48, 78, 38, 11, 34]
+        }
+      ]
+    });
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
